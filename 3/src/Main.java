a4d935800474ab887f75afb0ee1acc5412f7f58b
oliverrw93 <oliverrw93@DESKTOP-52US5H7.localdomain>
2021-12-04 00:05:25 +0000

    narrowed imports

--- 3/src/Main.java
+++ 3/src/Main.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
