4105b59954c81ef4a9ab6f87a3976327e54aa180
roboliver <oliver.rw93@gmail.com>
2021-12-05 20:35:48 +0000

    day 5 improvements

--- 05/src/Seafloor.java
+++ 05/src/Seafloor.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -5,35 +7,20 @@ import java.util.Set;
  */
 public class Seafloor {
 
-    private final int[][] grid;
-    // offsets we will use to compensate for the fact that the grid is indexed from 0 starting from the lowest y and x
-    // coordinates it will contain, but the vents are provided with their actual coordinates.
-    private final int xOffset;
-    private final int yOffset;
+    private final Map<Point, Integer> points = new HashMap<>();
     private int ventOverlaps = 0;
-
-    public Seafloor(int xMin, int xMax, int yMin, int yMax) {
-        if (xMin > xMax) {
-            throw new IllegalArgumentException("x min cannot be higher than x max");
-        } else if (yMin > yMax) {
-            throw new IllegalArgumentException("y min cannot be higher than y max");
-        }
-        int height = (yMax - yMin) + 1;
-        int width = (xMax - xMin) + 1;
-        grid = new int[height][width];
-        xOffset = xMin;
-        yOffset = yMin;
-    }
 
     public void addVent(Vent vent) {
         Set<Point> ventPoints = vent.points();
         for (Point point : ventPoints) {
-            int xForGrid = point.x() - xOffset;
-            int yForGrid = point.y() - yOffset;
-            if (grid[yForGrid][xForGrid] == 1) {
+            Integer count = points.get(point);
+            if (count == null) {
+                count = 0;
+            } else if (count.intValue() == 1) {
+                // found the first overlap at this point
                 ventOverlaps++;
             }
-            grid[yForGrid][xForGrid]++;
+            points.put(point, count + 1);
         }
     }
 
