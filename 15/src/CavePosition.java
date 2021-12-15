33cf1f5951bc63c6e7abcb241e410289cfadca5f
roboliver <oliver.rw93@gmail.com>
2021-12-15 21:44:53 +0000

    added comments and made algorithm clearer

--- 15/src/CavePosition.java
+++ 15/src/CavePosition.java
@@ -21,30 +21,42 @@ public class CavePosition {
         this.bestPathTo = isStart ? 0 : Integer.MAX_VALUE;
     }
 
+    /**
+     * Add a neighbour to this cave position, and also recalculate the best path to this position given the new route
+     * possibilities this neighbour provides.
+     * @param neighbour The neighbouring cave position.
+     */
     public void addNeighbour(CavePosition neighbour) {
         if (neighbour == this) {
             throw new IllegalArgumentException("a cave position can't neighbour itself");
         }
-        neighbours.add(neighbour);
+        // connect the positions to each other
+        this.neighbours.add(neighbour);
         neighbour.neighbours.add(this);
-        recalculateBestPath(neighbour);
+        // update the best route for this position and its new neighbour, now that they are joined
+        recalculateBestPathVia(neighbour);
+        neighbour.recalculateBestPathVia(this);
     }
 
-    private void recalculateBestPath(CavePosition neighbour) {
-        recalculateBestPath(this, neighbour);
-        recalculateBestPath(neighbour, this);
-    }
-
-    private static void recalculateBestPath(CavePosition to, CavePosition via) {
-        int pathToPositionVia = via.bestPathTo == Integer.MAX_VALUE ? Integer.MAX_VALUE : via.bestPathTo + to.chitons;
-        if (pathToPositionVia < to.bestPathTo) {
-            to.bestPathTo = pathToPositionVia;
-            for (CavePosition neighbour : to.neighbours) {
-                to.recalculateBestPath(neighbour);
+    private void recalculateBestPathVia(CavePosition via) {
+        // see if there is a better path to this position via the neighbour given as argument
+        int pathViaNeighbour = via.bestPathTo == Integer.MAX_VALUE ? Integer.MAX_VALUE : via.bestPathTo + this.chitons;
+        if (pathViaNeighbour < this.bestPathTo) {
+            // found one, so also update all this position's neighbours (excluding the one we got the route through,
+            // which would be pointless) in case this gives them a better route too
+            this.bestPathTo = pathViaNeighbour;
+            for (CavePosition neighbour : this.neighbours) {
+                if (neighbour != via) {
+                    neighbour.recalculateBestPathVia(this);
+                }
             }
         }
     }
 
+    /**
+     * Returns the best path from the starting position to this position.
+     * @return The best (lowest risk) path to this position.
+     */
     public int bestPathTo() {
         return this.bestPathTo;
     }
