71f8c8856cb264a0389e2b9f10cb34758ea0174f
roboliver <oliver.rw93@gmail.com>
2021-12-20 23:39:57 +0000

    day 19 progress

---
+++ 19/src/Beacon.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+
+public class Beacon {
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public Beacon(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public int z() {
+        return z;
+    }
+
+    public Beacon rotate(int pitch, int roll, int yaw) {
+        int xNew = requireValidRotate(pitch);
+        int yNew = requireValidRotate(roll);
+        int zNew = requireValidRotate(yaw);
+        for (int i = 0; i < pitch; i++) {
+            int zTemp = xNew;
+            xNew = zNew * -1;
+            zNew = zTemp;
+        }
+        for (int i = 0; i < roll; i++) {
+            int yTemp = zNew;
+            zNew = yNew * -1;
+            yNew = yTemp;
+        }
+        for (int i = 0; i < yaw; i++) {
+            int xTemp = yNew;
+            yNew = xNew * -1;
+            xNew = xTemp;
+        }
+        return new Beacon(xNew, yNew, zNew);
+    }
+
+    private static int requireValidRotate(int rotate) {
+        if (rotate < 0 || rotate > 3) {
+            throw new IllegalArgumentException("rotation value must be between 0 and 3");
+        }
+        return rotate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (!(obj instanceof Beacon)) {
+            return false;
+        } else {
+            return this.x == ((Beacon) obj).x
+                    && this.y == ((Beacon) obj).y
+                    && this.z == ((Beacon) obj).z;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int[] vals = {x, y, z};
+        return Arrays.hashCode(vals);
+    }
+}
---
+++ 19/src/Cluster.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Cluster {
+    private final Set<Beacon> beacons = new HashSet<>();
+
+    public void addBeacon(Beacon beacon) {
+        beacons.add(Objects.requireNonNull(beacon));
+    }
+
+    public Cluster rotate(int pitch, int roll, int yaw) {
+        Cluster rotated = new Cluster();
+        for (Beacon beacon : beacons) {
+            rotated.addBeacon(beacon.rotate(pitch, roll, yaw));
+        }
+        return rotated;
+    }
+}
