3f8b97b62067f7a419e2ba02d86ac0312d049224
roboliver <oliver.rw93@gmail.com>
2022-01-08 21:47:57 +0000

    closed reader in day 20 after reading input

--- 20/src/Main.java
+++ 20/src/Main.java
@@ -77,6 +77,7 @@ public class Main {
         for (int i = 0; i < imageList.size(); i++) {
             image[i] = imageList.get(i);
         }
+        lineReader.close();
         return image;
     }
 
