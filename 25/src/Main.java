693467e5c8a5e52f8c5498b81b315a7f42ae8488
roboliver <oliver.rw93@gmail.com>
2022-02-12 23:18:14 +0000

    day 25 skeleton

--- 25/src/Main.java
+++ 25/src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader lineReader = Utils.inputLineReader()) {
+            System.out.println("Step when sea cucumbers stop: " + stepWhenStopped(lineReader));
+        }
+    }
+
+    public static int stepWhenStopped(BufferedReader lineReader) throws IOException {
+        return 0;
+    }
 }
--- 25/test/MainTest.java
+++ 25/test/MainTest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    private static final String SAMPLE =
+            "v...>>.vv>\n"
+            + ".vv>>.vv..\n"
+            + ">>.>v>...v\n"
+            + ">>v>>.>.v.\n"
+            + "v>v.vv.v..\n"
+            + ">.>>..v...\n"
+            + ".vv..>.>v.\n"
+            + "v.v..>>v.v\n"
+            + "....v..v.>";
+
+    @Test
+    public void testStepWhenStopped() throws IOException {
+        assertEquals(58, Main.stepWhenStopped(Utils.testLineReader(SAMPLE)));
+    }
 }
